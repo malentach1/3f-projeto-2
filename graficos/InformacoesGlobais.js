a9f7a6c978c3cf2e2b9162df6130cf142ae12aaf
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-10-03 09:43:47 -0300

    Add files via upload

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -5,10 +5,18 @@ console.log(url);
 async function visualizaDadosGlobais(){
     const res = await fetch(url);
     const dados = await res.json();
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio;
     console.log(dados);
+
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-texto');
     const container = document.getElementById('graficos-container');
+    paragrafo.innerHTML = `O mundo tem ${totalPessoasMundo} bilhões de habitantes, 
+    dos quais ${totalPessoasConectadas} bilhões estão conectados em alguma rede social 
+    por um tempo médio de ${tempoMedio}.`;
+    container.appendChild(paragrafo);
 }
 
 visualizaDadosGlobais();
