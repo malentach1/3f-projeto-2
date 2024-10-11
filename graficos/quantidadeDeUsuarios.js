cedbcacb65eac053ca39cdb94a8e42eacd52d2c4
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-10-11 08:52:26 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -15,4 +15,12 @@ const infos = [
     }
 ]
 
+const grafico = document.createElement('div');
+grafico.className = 'grafico';
+document.getElementById('graficos-container').appendChild(grafico);
+Plotly.newPlot(grafico,infos);
+
+
 }
+
+quantidadeDeUsuarios();
