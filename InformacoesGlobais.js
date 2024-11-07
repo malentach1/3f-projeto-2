c590c172fc579abfff54ca64c74b3dac547e22a3
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-11-07 09:32:08 -0300

    Add files via upload

---
+++ InformacoesGlobais.js
@@ -0,0 +1,22 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+console.log(url);
+
+async function visualizaDadosGlobais(){
+    const res = await fetch(url);
+    const dados = await res.json();
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio;
+    console.log(dados);
+
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-texto');
+    const container = document.getElementById('graficos-container');
+    paragrafo.innerHTML = `O mundo tem <span> ${totalPessoasMundo} </span> bilhões de habitantes, 
+    dos quais <span> ${totalPessoasConectadas} </span> bilhões estão conectados em alguma rede social 
+    por um tempo médio de <span> ${tempoMedio} </span>.`;
+    container.appendChild(paragrafo);
+}
+
+visualizaDadosGlobais();
