1483a9585d7b01372f9f4a02032d7accd7f4f535
LarissaMalentchi <166127658+malentach1@users.noreply.github.com>
2024-11-08 08:46:08 -0300

    Add files via upload

---
+++ graficos/redesFavoritas.js
@@ -0,0 +1,22 @@
+import {getCSS, tickfont} from "./comum.js";
+
+async function redesFavoritas(){
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json';
+    const res = fetch(url);
+    const dados = (await res).json();
+    const redes = Object.keys(dados);
+    const valores = Object.values(dados);
+
+    const infos = [
+
+        {
+            values: valores,
+            labels: redes,
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+        
+    ]
+}
+
+redesFavoritas();
